7388b5fbd0065670ea4f5d283506cb587463de89
jinkanglow <61701779+jinkanglow@users.noreply.github.com>
2024-08-25 00:16:48 +0800

    Delete Backend directory

--- Backend/Middleware/Authorization.js
+++
@@ -1,9 +0,0 @@
-// middleware/authorizationMiddleware.js
-const authorize = (role) => (req, res, next) => {
-    if (!req.user || req.user.role !== role) {
-        return res.status(403).json({ message: 'Forbidden' });
-    }
-    next();
-};
-
-module.exports = authorize;
--- Backend/Middleware/Errors.js
+++
@@ -1,9 +0,0 @@
-// middleware/errorHandler.js
-const logger = require('../utils/logger');
-
-const errorHandler = (err, req, res, next) => {
-    logger.error(err.message, { stack: err.stack });
-    res.status(500).json({ message: 'Internal Server Error' });
-};
-
-module.exports = errorHandler;
